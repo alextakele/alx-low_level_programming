0fc34ef161608efb31cac7ea34cd60ee0d79d598
alexa221 <alextakele16@gmail.com>
2022-04-08 18:17:24 +0300

    Task 04

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,17 @@
  */
   int main(void)
 {
+puts("programming is like bulding amulti lingual puzzle");
+r#include <stdio.h>
+/**
+ *main -entry point
+ *
+ * Return 0: 0 after printing the functin
+ */
+  int main(void)
+{
 puts("\"programming is like bulding amulti lingual puzzle");
+return (0);
+}
 return (0);
 }
