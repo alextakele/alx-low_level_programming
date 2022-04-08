bc20e00912b9e9e22d8fdff214d2565a46554b8d
alexa221 <alextakele16@gmail.com>
2022-04-08 21:46:30 +0300

    Task 0

--- 0x01-variables_if_else_whil/0-positive_or_negative.c
+++ 0x01-variables_if_else_whil/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Prints a random number and states whether
  *        it is positive, negative, or zero.
@@ -10,17 +9,15 @@
 */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+return (0);
 }
--- 0x01-variables_if_else_whil/README.md
+++ 0x01-variables_if_else_whil/README.md
@@ -1,1 +1,2 @@
 readme file
+Conditional operators in C
