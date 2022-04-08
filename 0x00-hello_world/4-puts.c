d1445c4e5731f8fd6344fce3e30a110685628e69
alexa221 <alextakele16@gmail.com>
2022-04-08 18:08:13 +0300

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-# include  <studio.h>
+#include <stdio.h>
 /**
- * *main -entry point 
+ *main -entry point 
  * 
  * Return 0: 0 after printing the functin
  */
   int main(void)
 {
-	puts("\ "programming is like bulding amulti lingual puzzle"):
-		return (0):
+puts("\"programming is like bulding amulti lingual puzzle");
+return (0);
 }
