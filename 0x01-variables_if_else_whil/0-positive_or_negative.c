52ee9aef145775ec8467733989d7e115591bed9a
alexa221 <alextakele16@gmail.com>
2022-04-09 19:14:48 +0300

    Zero

--- 0x01-variables_if_else_whil/0-positive_or_negative.c
+++ 0x01-variables_if_else_whil/0-positive_or_negative.c
@@ -1,4 +1,4 @@
- /*
+/*
  * File: 0-positive_or_negative.c
  * Auth: Brennan D Baraban
  */
@@ -15,14 +15,14 @@
 */
 int main(void)
 {
-	int n;
+	int x;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	x = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (x> 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
+	else if (x < 0)
 		printf("%d is negative\n", n);
 	else
 		printf("%d is zero\n", n);
