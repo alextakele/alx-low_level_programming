4df1f54386048eeabafd460ed514f1a25b7f7c50
alexa221 <alextakele16@gmail.com>
2022-04-12 14:35:11 +0300

    Task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void);
+{
+        char alpha;
+
+        for (alpha = 'a'; alpha <= 'z'; alpha++)
+        {
+                _putchar(alpha);
+        }
+        _putchar('\n');
+}
