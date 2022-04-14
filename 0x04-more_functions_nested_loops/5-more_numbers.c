ac52704abe5df6dd8b5cab76e29a7edb8282beaa
alexa221 <alextakele16@gmail.com>
2022-04-14 13:20:40 +0300

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ void more_numbers(void)
 
 	while (b <= 9)
 	{
-		while (a <= 14)
-		{
-			if (a > 9)
-				_putchar(a / 10 + '0');
-			_putchar(a % 10 + '0');
-			a++;
-
-		}
+	while (a <= 14)
+	{
+	if (a > 9)
+		
+	_putchar(a / 10 + '0');
+	_putchar(a % 10 + '0');
+	a++;
+	}
 	_putchar('\n');
 	b++;
 	a = 0;
