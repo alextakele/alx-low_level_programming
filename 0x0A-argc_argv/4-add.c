fd7e7a82f438b5a212afaba6fc4209b4254c54f7
alexa221 <alextakele16@gmail.com>
2022-04-29 12:14:51 +0300

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _atoi(char *s);
+
+/**
+ * main - adds two strings, prints error if not digits in string
+ *
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 1 if error , 0 if no number, sum otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int a, i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		a = _atoi(argv[i]);
+		if (argv[i][0] != '0' && a == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += a;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * _atoi - return a num inside of a string
+ *
+ * @s: pointer for string to parse
+ * Return: int pulled from string, 0 if none
+ */
+int _atoi(char *s)
+{
+	unsigned int num = 0;
+	int flag = -1, i = 0, neg = 1;
+
+	if (*s == '\0')
+		flag = 1;
+	for (i = 0; flag == -1; i++)
+	{
+		if ((*(s + i) >= '0') && (*(s + i) <= '9'))
+		{
+			if (num != 0)
+				num = ((num * 10) + (*(s + i) - '0'));
+			else
+				num = (*(s + i) - '0');
+		}
+		else
+		{
+			return (0);
+		}
+		if (*(s + i + 1) == '\0')
+		{
+			flag = 1;
+		}
+	}
+	return (neg * num);
+}
