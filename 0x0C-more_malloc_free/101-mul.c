cca7efd5d27d1cb2e72ae0b49c83d28824cc7112
alexa221 <alextakele16@gmail.com>
2022-05-06 22:25:14 +0300

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,151 +1,213 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * _atoi_digit - convert a char to integer.
- * @x: character to convert.
- * Return: integer.
- **/
-
-int _atoi_digit(char x)
+ * add - addition of parcial products
+ * @p: parcial product
+ * @mg: big malloc
+ * @i: size of big malloc - 1
+ * @tp: size of parcial product - 1
+  */
+void add(char *p, char *mg, int i, int tp)
 {
-	unsigned int res;
+	int pos = tp, a = 0, b = 0, car = 0, res = 0;
+	int cont = tp, aux = 0;
 
-	if (x <= '9' && x >= '0')
-		res = x - '0';
-	return (res);
+	for (; cont >=  0; cont--, i--, pos--)
+	{
+		a = *(p + pos) - '0';
+		b = *(mg + i) - '0';
+		res = (a + b) + car;
+		car = res / 10;
+		res = (res % 10) + '0';
+		*(mg + i) = res;
+	}
+	res = 0;
+	for (aux = i - 1; aux >= 0; aux--)
+	{
+		a = *(mg + aux) - '0';
+		a += a + car;
+		car = a / 10;
+		a = (a % 10) + '0';
+		*(mg + aux) = a;
+	}
 }
-
 /**
- * _isNumber - Define if a string is a number.
- * @argv: Pointer to string.
- * Return: success (0).
- **/
-int _isNumber(char *argv)
+ * setiar - fill the pointer with 0
+ * @s: pointer
+ * @a: size of pointer
+ */
+void setiar(char *s, int a)
 {
 	int i;
 
-	for (i = 0; argv[i]; i++)
-		if (argv[i] < 48 || argv[i] > 57)
-			return (1);
-	return (0);
+	for (i = 0; i < a; i++)
+		s[i] = '0';
 }
-
 /**
- *_calloc - allocate array of size * nmemb.
- * @nmemb: number of elements.
- * @size: size of element.
- * Return: pointer to array.
- **/
-
-void *_calloc(unsigned int nmemb, unsigned int size)
+ * parcial - give the parcial malloc
+ * @a: size one
+ * Return: parcial malloc
+ */
+char *parcial(int a)
 {
-	char *tab;
-	unsigned int i;
+	int i;
+	char *p;
 
-	tab = malloc(size * nmemb);
-
-	if (tab == NULL)
+	p = malloc(a);
+	if (!p)
 		return (NULL);
-
-	for (i = 0; i < (size * nmemb); i++)
-		tab[i] = '0';
-
-	return (tab);
+	for (i = 0; i < a; i++)
+		p[i] = '0';
+	return (p);
 }
-
 /**
- * mul_array - multiply two arrays.
- * @a1: first array.
- * @len1: length of array a1.
- * @a2:  char.
- * @a3: array for result.
- * @lena: length of array a3.
- * Return: pointer to array.
- **/
-
-void *mul_array(char *a1, int len1, char a2, char *a3, int lena)
+ * grande - give big malloc
+ * @a: size one
+ * @b: size two
+ * Return: malloc
+ */
+char *grande(int a, int b)
 {
-	int mul = 0, i, k;
+	int i;
+	char *p;
 
-	k = lena;
-	for (i = len1 - 1; i >= 0 ; i--)
-	{
-		mul += (a1[i] - '0') * (a2 - '0') + (a3[k] - '0');
-		a3[k] = (mul % 10) + '0';
-		mul /= 10;
-		k--;
-	}
-
-		while (mul != 0)
-		{
-			mul += a3[k] - '0';
-			a3[k] = (mul % 10) + '0';
-			mul /= 10;
-			k--;
-		}
-
-	return (a3);
+	p = malloc(a + b);
+	if (!p)
+		return (NULL);
+	for (i = 0; i < a + b; i++)
+		p[i] = '0';
+	return (p);
 }
 /**
- * print_array - print all digits of array.
- * @nb: number of elements to print.
- * @a: array of elements.
- **/
-void print_array(char *a, int nb)
+ * tama - give the size
+ * @s: string of numbers
+ * Return: size
+ */
+int tama(char *s)
 {
 	int i = 0;
 
-	while (a[i] == '0' && (i + 1) < nb)
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for (; i < nb; i++)
+	return (i);
+}
+/**
+ * Perror - print Error with putchar.
+ * Return: Always 0.
+ */
+void Perror(void)
+{
+	_putchar('E');
+	_putchar('r');
+	_putchar('r');
+	_putchar('o');
+	_putchar('r');
+	_putchar('\n');
+	exit(98);
+}
+/**
+ * numero - check if strings have only numbers.
+ * @s: string 1
+ * @a: string 2
+ * Return: Always 1 if not or 0 if only has number.
+ */
+void numero(char *s, char *a)
+{
+	int i = 0, aux = 0;
+
+	while (s[i] != '\0')
 	{
-		_putchar(a[i]);
+		if (!(s[i] >= '0' && s[i] <= '9'))
+		{
+			aux = 1;
+			break;
+		}
+		i++;
+	}
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if (!(a[i] >= '0' && a[i] <= '9'))
+		{
+			aux = 1;
+			break;
+		}
+		i++;
+	}
+	if (aux == 1)
+		Perror();
+	if (*s == '0' || *a == '0')
+	{
+		_putchar('0');
+		_putchar('\n');
+		exit(0);
+	}
+}
+/**
+ * impresion - print the pointer
+ * @s: string.
+ * @a: size of string
+ */
+void impresion(char *s, int a)
+{
+	int i;
+
+	for (i = 0; i < a; i++)
+	{
+		if (!(s[0] == '0' && i == 0))
+			_putchar(s[i]);
 	}
 	_putchar('\n');
 }
-
 /**
- *main - print the multiplication of 2 numbers.
- *@argc: array length.
- *@argv: array.
- *Return: 0.
+ * main - check the code for Holberton School students.
+ * @argv: array of pointers store the arguments
+ * @argc: quantity of arguments
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int i, c, len1, len2, lenres;
-	char E[6] = {'E', 'r', 'r', 'o', 'r', '\n'};
-	char *tabres;
+	int t1, t2, t3, a = 0, b = 0, j = 0, aux = 0, car, g, ind;
+	char *mg, *p, *size1, *size2;
 
-	if (argc != 3 || _isNumber(argv[1]) == 1 || _isNumber(argv[2]) == 1)
-	{
-		for (i = 0; i < 6; i++)
-		{
-			_putchar(E[i]);
-		}
-		exit(98);
-	}
-	for (len1 = 0; argv[1][len1]; len1++)
-	;
-	for (len2 = 0; argv[2][len2]; len2++)
-	;
-	lenres = len1 + len2;
-	tabres = _calloc(lenres, sizeof(int));
-	if (tabres == NULL)
-	{
-		free(tabres);
-		return (0);
-	}
-	for (i = len2 - 1, c = 0; i >= 0; i--)
-	{
-	tabres = mul_array(argv[1], len1, argv[2][i], tabres, (lenres - 1 - c));
-	c++;
-	}
-	print_array(tabres, lenres);
-	free(tabres);
-	exit(EXIT_SUCCESS);
+	if (argc != 3)
+		Perror();
+	else
+	{ numero(argv[1], argv[2]);
+		t1 = tama(argv[1]);
+		t2 = tama(argv[2]);
+		ind = t1 + t2;
+		mg = grande(t1, t2);
+		if (t1 >= t2)
+		{ size1 = argv[1];
+			size2 = argv[2];
+			b = t1 - 1;
+			a = t2 - 1;
+			t3 = t1 + 1; }
+		else
+		{ size1 = argv[2];
+			size2 = argv[1];
+			b = t2 - 1;
+			a = t1 - 1;
+			t3 = t2 + 1; }
+		g = b;
+		p = parcial(t3);
+		j = b + 1;
+		for (; a >= 0; a--, ind--)
+		{ car = 0;
+			b = g;
+			j = g + 1;
+			setiar(p, t3);
+			for (; b >= 0; b--, j--)
+			{ aux = ((size2[a] - '0') * (size1[b] - '0'));
+				aux = aux + car;
+				p[j] = ((aux % 10) + '0');
+				car = aux / 10;	}
+			p[j] = (car + '0');
+			add(p, mg, (ind - 1), (t3 - 1)); }
+		impresion(mg, (t1 + t2)); }
 	return (0);
 }
