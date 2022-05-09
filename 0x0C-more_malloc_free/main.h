148b6e98c05b438a13be7dce33685907e1582a75
alexa221 <alextakele16@gmail.com>
2022-05-09 16:46:34 +0300

    exit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,31 +1,18 @@
-#ifndef MAIN_FUNTIONS
-#define MAIN_FUNTIONS
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * struct add_s - Node in the linked list of additions
- *
- * @n_add: Pointer to an addition representation
- * @n_dig: Real space that addtion takes
- * @len_r: Total space to save the addition
- * @next: Pointer to the next addition
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes for all functions
+        used in the 0x0C-more_malloc_free directory.
  */
-typedef struct add_s
-{
-	char *n_add;
-	int n_dig;
-	int len_r;
-	struct add_s *next;
-} add_t;
 
-
-int _putchar(char c);  /* An example function declaration */
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif
