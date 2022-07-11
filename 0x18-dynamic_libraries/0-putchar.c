f600da6a7fdd9adb04a06846ce66585e42f6288f
alexa221 <alextakele16@gmail.com>
2022-07-11 16:32:59 +0300

    dynamic libraries

---
+++ 0x18-dynamic_libraries/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+
+int _putchar(char c)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+ printf("something you want..,\n");
+return (0);
+}
